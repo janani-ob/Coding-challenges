841fe1f617581d38c16d55dbc738168707418411
janani ob <jananiob@gmail.com>
2022-10-18 09:59:44 -0700

    program upadted with content

--- javascript/hoisting.js
+++ javascript/hoisting.js
@@ -1,3 +1,6 @@
+//Hoisting is a phenomenon that accessing some variables and before even initializing or putting some values into them without getting error.
+//placing the code snippetes in different parts of the program and the execution of ot gave a ckear view of what is hoisting in javascipt.
+
 var x=7;
 function getName(){
     console.log("Namaste Javascript");
@@ -5,3 +8,6 @@ function getName(){
 
 getName();
 console.log(x);
+
+
+// also the demo of call stack has been teached, which was really interesting.
